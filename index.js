0c2bc62bdd14296dd2a2da3329d3a29e9494750d
Rafay Jamal <rafayj34@gmail.com>
2025-11-06 01:20:28 -0500

    feat: Add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello, World to devops!');
+console.log('Github actions for CI/CD');
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('Hello, World!');
+console.log('This is the test.js file.');
+setTimeout(() => {
+    console.log('Testing asynchronous code.');
+}, 3000);
+console.log('Testing complete');
