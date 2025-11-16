b6de092fac9d0558c26b9b803481f35c0840b958
Rafay Jamal <rafayj34@gmail.com>
2025-11-16 17:23:46 -0500

    Setup ESLint and Prettier

--- test.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello, World!');
-console.log('This is the test.js file.');
-setTimeout(() => {
-    console.log('Testing asynchronous code.');
-}, 3000);
-console.log('Testing complete');
